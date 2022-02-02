58f20ba343ae6ae14ff4a00142245902226e5bf8
KIM DONG IL <dongil.kim@powertask.biz>
2022-02-02 12:03:07 +0900

    class

---
+++ 4-2-class.ts
@@ -0,0 +1,39 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  class CoffeeMaker {
+    static BEANS_GRAMM_PER_SHOT: number = 7;
+    coffeeBeans: number = 0; // gram 단위
+
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMaker {
+      return new CoffeeMaker(coffeeBeans);
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
+        throw new Error("커피콩이 충분하지 않아요");
+      }
+
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+  }
+
+  const maker = new CoffeeMaker(32);
+  console.log(maker);
+  const maker2 = new CoffeeMaker(10);
+  console.log(maker2);
+
+  const maker3 = CoffeeMaker.makeMachine(3);
+  console.log(maker3);
+}
